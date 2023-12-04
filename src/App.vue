37db4ca705b222ff502c805d780e56eceb9a71b0
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-04 14:14:41 -0300

    :sparkles: feat: start project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-layout>
+    <v-app-bar tutle="Omlyy Serviços Especializados">
+        Application Bar
+    </v-app-bar>
+
+    <v-main class="w-full h-full">
+        Main Content
+    </v-main>
+  </v-layout>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
