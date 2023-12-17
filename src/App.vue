4f2e9af395f879d77e3a3600c87ec820eddf2040
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-12-17 15:32:34 -0300

    :lipstick: style: estilização do header da landing page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
     <v-layout>
-        <v-app-bar title="Omlyy Serviços Especializados"> 
+        <v-app-bar :elevation="0" title="Omlyy Serviços Especializados">
+            <v-spacer></v-spacer>
+            <v-btn>Home</v-btn>
+            <v-btn>Sobre</v-btn>
+            <v-btn>Contato</v-btn>
         </v-app-bar>
 
-        <v-main class="w-full h-full">
-            Main Content
-        </v-main>
+        <v-main class="w-full h-full"> Main Content </v-main>
     </v-layout>
 </template>
